cfa66b1a04b2f81b5ce4c5d795b0f88e157da630
martinristow <martinristov084@gmail.com>
2025-03-01 18:15:45 +0100

    Add lifespan and call init_db function

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from src.books import routes
+from contextlib import asynccontextmanager
+from src.db.main import init_db
+
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    print("server is starting...")
+    await init_db()
+    yield
+    print("server has been stopped!")
+
+
+version = "v1"
+app = FastAPI(
+    title="Bookly",
+    description="A REST API for a book review web service",
+    version=version,
+    lifespan=life_span
+)
+
+app.include_router(routes.router, prefix="/books")
+
+
+@app.get("/")
+async def home():
+    return {"message": "hello world"}
