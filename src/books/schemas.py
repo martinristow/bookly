70bfcd38827e1bf2f1b699c7b7448686ffb07b2e
martinristow <martinristov084@gmail.com>
2025-03-01 18:57:28 +0100

    Add schemas for Book, CreateBook and UpdateBook

--- src/books/schemas.py
+++ src/books/schemas.py
@@ -1,6 +1,28 @@
 from pydantic import BaseModel
+from datetime import datetime
+import uuid
 
 
-class CreateBook(BaseModel):
+class Book(BaseModel):
+    uid: uuid.UUID
     title: str
     author: str
+    publisher: str
+    published_data: datetime
+    page_count: int
+    language: str
+    created_at: datetime
+    update_at: datetime
+
+
+class BookCreateModel(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    published_data: datetime
+    page_count: int
+    language: str
+
+
+class BookUpdateModel(BookCreateModel):
+    pass
