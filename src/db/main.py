c66c0d3f2147b81908c3e5eda79872db4f46cb7f
martinristow <martinristov084@gmail.com>
2025-03-02 15:28:21 +0100

    Refactor database initialization and session management
    - Replaced `create_engine` with `create_async_engine` for async database connection
    - Updated database initialization to use async context with `async_engine`
    - Added a new `get_session` function to manage database sessions asynchronously using `AsyncSession` and `sessionmaker`

--- src/db/main.py
+++ src/db/main.py
@@ -1,22 +1,27 @@
-from sqlmodel import create_engine, text, SQLModel
-from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlmodel import create_engine, SQLModel
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import sessionmaker
 from src.config import settings
 
-
-engine = AsyncEngine(create_engine(
-    url=settings.DATABASE_URL,
-    echo=True
-))
+# Create async engine using create_async_engine
+async_engine = create_async_engine(settings.DATABASE_URL, echo=True)
 
 
 async def init_db():
-    async with engine.begin() as conn:
-        # This code is for testing connection with database
-        # statement = text("SELECT 'hello';")
-        #
-        # result = await conn.execute(statement)
-        #
-        # print(result.all())
+    async with async_engine.begin() as conn:
         from src.books.models import Book
-        await conn.run_sync(SQLModel.metadata.create_all)
+        # await conn.run_sync(SQLModel.metadata.create_all)
 
+
+async def get_session() -> AsyncSession:
+    # Use sessionmaker with correct type
+    Session = sessionmaker(
+        bind=async_engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+    )
+
+    # Ensure we yield the session in an async context
+    async with Session() as session:
+        yield session
