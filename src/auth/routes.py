54fd972f377a7472f6da8841731bc5120b4c27c9
martinristow <martinristov084@gmail.com>
2025-03-04 00:26:34 +0100

    Make object from UserService class and call a function create_user into create_user_account function

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,9 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from .schemas import UserCreateModel
+from .service import UserService
+from sqlmodel.ext.asyncio.session import AsyncSession
+from src.db.main import get_session
+
+user = UserService()
+
 
 router = APIRouter(tags=["auth"])
 
 
-@router.post("/signup")
-async def create_user_account(user_data: UserCreateModel):
-    pass
+@router.post("/signup", response_model=UserCreateModel)
+async def create_user_account(user_data: UserCreateModel, session: AsyncSession = Depends(get_session)) -> dict:
+
+    new_user = await user.create_user(user_data, session)
+
+    return new_user
