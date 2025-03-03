e865e2514d0732dc339c205aec6b9db5bc634210
martinristow <martinristov084@gmail.com>
2025-03-03 18:59:48 +0100

    Initial Commit

---
+++ src/auth/models.py
@@ -0,0 +1,30 @@
+from sqlmodel import SQLModel, Column, Field
+from datetime import datetime
+import uuid
+import sqlalchemy.dialects.postgresql as pg
+
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+
+    uid: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            nullable=False,
+            primary_key=True,
+            default=uuid.uuid4
+        )
+    )
+
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    is_verified: bool = Field(default=False)
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now()))
+    update_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now()))
+
+
+    def __repr__(self):
+        return f"<User {self.username}>"
