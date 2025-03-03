a645b6f860e133e26b977cbcdb6eb13ae62ca58f
martinristow <martinristov084@gmail.com>
2025-03-04 00:24:29 +0100

    Add new fields in schemas

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -4,4 +4,6 @@ from pydantic import BaseModel, Field, EmailStr
 class UserCreateModel(BaseModel):
     username: str = Field(min_length=3, max_length=8)
     email: EmailStr = Field(max_length=40)
-    password: str = Field(min_length=6, max_length=20)
+    first_name: str
+    last_name: str
+    password_hash: str = Field(min_length=6, max_length=20)
