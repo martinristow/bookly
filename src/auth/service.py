7efc9b2dd05d31854506b2f1ab8d5dabb2cec615
martinristow <martinristov084@gmail.com>
2025-03-04 00:25:43 +0100

    Create class UserService that contains a few functions: get_user_by_email, user_exists and create_user

---
+++ src/auth/service.py
@@ -0,0 +1,34 @@
+from .models import User
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlmodel import select
+from .schemas import UserCreateModel
+
+
+class UserService:
+    async def get_user_by_email(self, email: str, session: AsyncSession):
+        statement = select(User).where(User.email == email)
+
+        result = await session.exec(statement)
+
+        user = result.first()
+
+        return user
+
+    async def user_exists(self, email, session: AsyncSession):
+        user = await self.get_user_by_email(email, session)
+
+        return user if user is not None else False
+
+    async def create_user(self, user_data: UserCreateModel, session: AsyncSession):
+        user_data_dict = user_data.model_dump()
+
+        new_user = User(
+            **user_data_dict
+        )
+
+        session.add(new_user)
+        await session.commit()
+
+        await session.refresh(new_user)
+
+        return new_user
