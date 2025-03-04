b5daa88c18dfffa12c2422390d162f23bd62805c
martinristow <martinristov084@gmail.com>
2025-03-04 13:13:16 +0100

    Add a new model that will be inherited by other classes

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,9 +1,23 @@
 from pydantic import BaseModel, Field, EmailStr
+from datetime import datetime
+import uuid
 
 
-class UserCreateModel(BaseModel):
+class UserModel(BaseModel):
     username: str = Field(min_length=3, max_length=8)
     email: EmailStr = Field(max_length=40)
-    first_name: str
-    last_name: str
-    password_hash: str = Field(min_length=6, max_length=20)
+    first_name: str = Field(max_length=25)
+    last_name: str = Field(max_length=25)
+    password_hash: str = Field(min_length=6)
+
+
+class UserCreateModel(UserModel):
+    pass
+
+
+class UserResponseModel(UserModel):
+    uid: uuid.UUID
+    is_verified: bool
+    password_hash: str = Field(exclude=True)
+    created_at: datetime
+    update_at: datetime
