139b093143e49a7c603799b2bc0f6ef8b3791930
martinristow <martinristov084@gmail.com>
2025-03-04 13:15:44 +0100

    Check if the user exists and raise an exception if it does

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,18 +1,23 @@
-from fastapi import APIRouter, Depends
-from .schemas import UserCreateModel
+from fastapi import APIRouter, Depends, HTTPException, status
+from .schemas import UserCreateModel, UserResponseModel
 from .service import UserService
 from sqlmodel.ext.asyncio.session import AsyncSession
 from src.db.main import get_session
 
-user = UserService()
-
+user_service = UserService()
 
 router = APIRouter(tags=["auth"])
 
 
-@router.post("/signup", response_model=UserCreateModel)
+@router.post("/signup", response_model=UserResponseModel, status_code=status.HTTP_201_CREATED)
 async def create_user_account(user_data: UserCreateModel, session: AsyncSession = Depends(get_session)) -> dict:
+    email = user_data.email
 
-    new_user = await user.create_user(user_data, session)
+    user_exists = await user_service.user_exists(email, session)
+
+    if user_exists:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"User with email: {email} already exists")
+
+    new_user = await user_service.create_user(user_data, session)
 
     return new_user
