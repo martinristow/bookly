bdea5b7f6ec2dbf48dfe6c27f366a8aa1e8e7a03
martinristow <martinristov084@gmail.com>
2025-03-06 00:36:57 +0100

    Forgot to add parenthesis in token_valid function and provide token like a argument

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -15,7 +15,7 @@ class TokenBearer(HTTPBearer):
 
         token_data = decode_token(token)
 
-        if not self.token_valid:
+        if not self.token_valid(token):
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid or expired token")
 
         self.verify_token_data(token_data)
