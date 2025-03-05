02d92e0a65d8ceb477268dab93fd9bd02bfd9b9b
martinristow <martinristov084@gmail.com>
2025-03-05 23:54:51 +0100

    Add AccessTokenBearer and RefreshTokenBearer classes. Add function verify_token_data in base class.

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -4,16 +4,12 @@ from fastapi.security.http import HTTPAuthorizationCredentials
 from .utils import decode_token
 
 
-class AccessTokenBearer(HTTPBearer):
+class TokenBearer(HTTPBearer):
     def __init__(self, auto_error=True):
         super().__init__(auto_error=auto_error)
 
     async def __call__(self, request: Request) -> HTTPAuthorizationCredentials | None:
         credentials = await super().__call__(request)
-        #
-        # print(credentials.scheme)
-        #
-        # print(credentials.credentials)
 
         token = credentials.credentials
 
@@ -22,8 +18,7 @@ class AccessTokenBearer(HTTPBearer):
         if not self.token_valid:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid or expired token")
 
-        if token_data['refresh']:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="PLease provide an access token")
+        self.verify_token_data(token_data)
 
         return token_data
 
@@ -31,3 +26,20 @@ class AccessTokenBearer(HTTPBearer):
         token_data = decode_token(token)
 
         return True if token_data is not None else False
+
+
+    def verify_token_data(self, token_data):
+        raise NotImplementedError("Please Override this method in child classes")
+
+
+class AccessTokenBearer(TokenBearer):
+
+    def verify_token_data(self, token_data: dict) -> None:
+        if token_data and token_data['refresh']:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="PLease provide an access token")
+
+
+class RefreshTokenBearer(TokenBearer):
+    def verify_token_data(self, token_data: dict) -> None:
+        if token_data and not token_data['refresh']:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="PLease provide a refresh token")
