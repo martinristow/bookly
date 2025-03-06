056b365005e69daa9b160ac2c57aa58348d57fed
martinristow <martinristov084@gmail.com>
2025-03-06 14:42:21 +0100

    Add new async function to get current user via token_details -> AccessTokenBearer

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -1,7 +1,13 @@
 from fastapi.security import HTTPBearer
-from fastapi import Request, HTTPException, status
+from fastapi import Request, HTTPException, status, Depends
 from fastapi.security.http import HTTPAuthorizationCredentials
 from .utils import decode_token
+from src.db.main import get_session
+from sqlmodel.ext.asyncio.session import AsyncSession
+from .service import UserService
+
+
+user_service = UserService()
 
 
 class TokenBearer(HTTPBearer):
@@ -16,7 +22,11 @@ class TokenBearer(HTTPBearer):
         token_data = decode_token(token)
 
         if not self.token_valid(token):
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid or expired token")
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail={
+                "error": "This token is invalid or expired",
+                "resolution": "Please get new token"
+            })
+
 
         self.verify_token_data(token_data)
 
@@ -25,8 +35,7 @@ class TokenBearer(HTTPBearer):
     def token_valid(self, token: str) -> bool:
         token_data = decode_token(token)
 
-        return True if token_data is not None else False
-
+        return token_data is not None
 
     def verify_token_data(self, token_data):
         raise NotImplementedError("Please Override this method in child classes")
@@ -43,3 +52,12 @@ class RefreshTokenBearer(TokenBearer):
     def verify_token_data(self, token_data: dict) -> None:
         if token_data and not token_data['refresh']:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="PLease provide a refresh token")
+
+
+
+async def get_current_user(token_details: dict = Depends(AccessTokenBearer()), session: AsyncSession = Depends(get_session)):
+    user_email = token_details['user']['email']
+
+    user = await user_service.get_user_by_email(user_email, session)
+
+    return user
