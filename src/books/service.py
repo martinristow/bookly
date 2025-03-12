3f1e9d2001c0a7ff83b5557c0942fe52c9eb2923
martinristow <martinristov084@gmail.com>
2025-03-12 12:43:07 +0100

    Add a new async function to return all user books

--- src/books/service.py
+++ src/books/service.py
@@ -12,6 +12,13 @@ class BookService:
 
         return result.all()
 
+    async def get_user_books(self, user_uid: str, session: AsyncSession):
+        statement = select(Book).where(Book.user_uid == user_uid).order_by(desc(Book.created_at))
+
+        result = await session.exec(statement)
+
+        return result.all()
+
     async def get_book(self, book_uid: str, session: AsyncSession):
         statement = select(Book).where(Book.uid == book_uid)
 
@@ -21,12 +28,14 @@ class BookService:
 
         return book if book is not None else None
 
-    async def create_book(self, book_data: BookCreateModel, session: AsyncSession):
+    async def create_book(self, book_data: BookCreateModel, user_uid: str, session: AsyncSession):
         book_data_dict = book_data.model_dump()
 
         new_book = Book(
             **book_data_dict
         )
+
+        new_book.user_uid = user_uid
 
         session.add(new_book)
 
